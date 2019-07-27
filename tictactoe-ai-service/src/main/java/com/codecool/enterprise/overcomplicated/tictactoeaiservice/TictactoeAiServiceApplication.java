b5e1439f0c4b2115bba65d11b76d800435ef24c7
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-07-27 16:22:46 +0200

    intial

---
+++ tictactoe-ai-service/src/main/java/com/codecool/enterprise/overcomplicated/tictactoeaiservice/TictactoeAiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.enterprise.overcomplicated.tictactoeaiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeAiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TictactoeAiServiceApplication.class, args);
+    }
+
+}
